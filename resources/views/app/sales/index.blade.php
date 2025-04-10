008a9638c82da145d3b2b61a0fee9d9453737941
khalidexordium@gmail.com <khalidexordium@gmail.com>
2025-04-10 22:28:36 +0500

    add button design chage

--- resources/views/app/sales/index.blade.php
+++ resources/views/app/sales/index.blade.php
@@ -34,8 +34,13 @@
                     <div class="p-6 text-gray-900">
                         <div class="flex justify-between items-center mb-6">
                             <h2 class="text-2xl font-bold">Sales Transactions</h2>
-                            <a href="{{ route('sales.create') }}" class="btn btn-primary">
-                                New Sale
+
+                            <a href="{{ route('sales.create') }}" class="sidebar-item {{ request()->routeIs('products.create') ? 'active' : '' }} flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 448 512">
+                                    <path class="fill-black-600" d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32z"/>
+                                    <path class="fill-white" d="M200 344l0-64-64 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l64 0 0-64c0-13.3 10.7-24 24-24s24 10.7 24 24l0 64 64 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-64 0 0 64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/>
+                                </svg>
+                                <span class="text">New Sale</span>
                             </a>
                         </div>
 
